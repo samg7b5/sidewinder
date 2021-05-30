2b7e13beb79b8c5b500eb4824b07de9f2659c5c1
samg7b5 <51542511+samg7b5@users.noreply.github.com>
2021-05-30 16:26:01 +0100

    Create snippets.py

---
+++ sidewinder/snippets.py
@@ -0,0 +1,67 @@
+from sidewinder.utilities import parse_progression
+
+## CHUNKS
+
+# could generate/generalise these e.g. two_five_one is implicitly the II, V and I chords from the major scale
+two_five = 'ii-7, V7'
+two_five_one = two_five + ', IM7' # returns list
+three_six_two_five_one = 'iii-7, vi-7, ' + two_five_one
+minor_two_five = 'iim7b5, V7'
+minor_two_five_one = minor_two_five + ', im/M7'
+minor_two_five_one_m7 = minor_two_five + ', i-7'
+twelve_bar_blues = 'I7, IV7, I7, V7, IV7, I7'
+major_to_minor = 'IM7, i-7'
+dominant_two_to_minor_two = 'II7, ii-7'
+dominant_in_fourths = 'I7, IV7'
+sixth_relation = 'IM7, VI7' # could sub VI7 -> #io7 which functions like a VI7b9 (e.g. AC#EG -> C#EGBb)
+one_to_flat_two = 'IM7, bIIM7' # bII is from parallel Phrygian, or tritone sub of VMaj7/bvii-7 (functions as Neapolitan subdominant), see https://www.youtube.com/watch?v=yF49xvxaaBY
+half_step_dominants_up = 'I7, bII7'
+half_step_dominants_down = 'I7, VII7'
+minor_third_dominant = 'IMaj7, bIII7'
+tonic_to_half_dim_seven = 'IM7, VIIm7b5'
+
+
+CHUNKS = {k:parse_progression(v) for k,v in 
+    {
+        "251": two_five_one,
+        "36251": three_six_two_five_one,
+        "minor 251": minor_two_five_one,
+        "minor 251 m7": minor_two_five_one_m7,
+        "12 bar blues": twelve_bar_blues,
+        "major to minor": major_to_minor,
+        "II7 to ii7": dominant_two_to_minor_two,
+        "I7 to IV7": dominant_in_fourths,
+        "tonic to six dominant": sixth_relation,
+        "tonic to flat two": one_to_flat_two,
+        "half step dominants up": half_step_dominants_up,
+        "half step dominants down": half_step_dominants_down,
+        "tonic to b3 dominant": minor_third_dominant,
+        "tonic to VII half dim": tonic_to_half_dim_seven,
+    }.items()
+}
+
+## STANDARDS
+
+misty = 'IM7, v-7, I7, IVM7, iv-9, bVII7, IM7, vi-7, ii-7, V7, iii-7, VI7, ii-7, V7, \
+            IM7, v-7, I7, IVM7, iv-9, bVII7, IM7, vi-7, ii-7, V7, I6, bVII9, IM7, \
+            v-7, I7b9, IVM7, IVM7,\
+            bv-7, VII7, II7, iii-7, VI7b9, ii-7, V7, \
+            IM7, v-7, I7, IVM7, iv-9, bVII7, IM7, vi-7, ii-7, V7, I6, I6'
+
+misty_durs = [1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 
+            1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 
+            1, 1, 1, 1, 
+            1, 2, 2, 2, 2, 2, 2, 
+            1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1]
+
+# could be simplified
+yardbird_suite = "A, D-, G7, A7, G7, F#7, B7, E7, C#-, F#7, F#7,\
+                  A, D-, G7, A7, G7, F#7, B7, E7, A7, A, G#7+9, \
+                  C#-. D#m7b5, G#7+9, C#-, F#7, B-, C#m7b5, F#7, B7, B7, Bb7, \
+                  A, D-, G7, A7, G7, F#7, B7, E7, C#-, F#7, B-, E7, A, D-, G7"
+
+yardbird_suite_durs = [1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 1, 
+                       1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 
+                       1, 2, 2, 1, 1, 1, 2, 2, 1, 2, 2, 
+                       1, 2, 2, 2, 2, 1, 1, 0.5, 2, 2, 
+                       1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2]
