35cadf88a93e312142691098cbbaed8ee1e9d66a
samg7b5 <51542511+samg7b5@users.noreply.github.com>
2021-06-05 18:55:36 +0100

    Add snippets

--- sidewinder/snippets.py
+++ sidewinder/snippets.py
@@ -4,7 +4,8 @@ from sidewinder.utilities import parse_progression
 
 # could generate/generalise these e.g. two_five_one is implicitly the II, V and I chords from the major scale
 two_five = 'ii-7, V7'
-two_five_one = two_five + ', IM7' # returns list
+two_five_one = two_five + ', IM7'
+two_five_one_four = two_five_one + ', IVM7'
 three_six_two_five_one = 'iii-7, vi-7, ' + two_five_one
 minor_two_five = 'iim7b5, V7'
 minor_two_five_one = minor_two_five + ', im/M7'
@@ -23,10 +24,12 @@ tonic_to_half_dim_seven = 'IM7, VIIm7b5'
 
 CHUNKS = {k:parse_progression(v) for k,v in 
     {
+        "25": two_five,
         "251": two_five_one,
+        "2514": two_five_one_four,
         "36251": three_six_two_five_one,
         "minor 251": minor_two_five_one,
-        "minor 251 m7": minor_two_five_one_m7,
+        "minor 251m7": minor_two_five_one_m7,
         "12 bar blues": twelve_bar_blues,
         "major to minor": major_to_minor,
         "II7 to ii7": dominant_two_to_minor_two,
@@ -39,6 +42,18 @@ CHUNKS = {k:parse_progression(v) for k,v in
         "tonic to VII half dim": tonic_to_half_dim_seven,
     }.items()
 }
+
+## SUBSITUTIONS
+backdoor_two_five = {'ii-7, V7': 'iv-7, bVII7'}
+shelton_berg_backdoor = {'#ivm7b5, VII7, IM9': '#ivm7b5, VII7, iii-7'}
+tritone_sub = {'V7': 'bII7'}
+tritone_1625 = {'I, VI7, ii-7, V7': 'I, bIII7, ii-7, bII7'} # descending root like in Oleo (Coltrane)
+
+CHORD_SUBS = {
+    "tritone": tritone_sub,
+    "backdoor 25": backdoor_two_five,
+}
+
 
 ## STANDARDS
 
